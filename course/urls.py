684e40a9741d37401b75443112b006d40a76c62b
theodimi404 <theodimitriades@gmail.com>
2023-12-16 22:41:08 +0200

    feat(course api): change the url name to map the view name

--- course/urls.py
+++ course/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from course.views import CourseListAndCreate, CourseOperationsById
 
 urlpatterns = [
-    path('', CourseListAndCreate.as_view(), name='get-all-courses'),
+    path('', CourseListAndCreate.as_view(), name='course-list-and-create'),
     path('<int:course_id>', CourseOperationsById.as_view(), name='course-operations-by-id'),
 ]
